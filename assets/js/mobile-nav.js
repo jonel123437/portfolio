0deb7f24bf69c3195b8da37a0ebdc166106f60cc
jonel123437 <145882424+jonel123437@users.noreply.github.com>
2024-02-17 15:58:12 +0800

    updated

--- assets/js/mobile-nav.js
+++ assets/js/mobile-nav.js
@@ -1,8 +1,28 @@
 const mobileNav = () => {
-    const headerbtn = document.querySelector('header__bars');
-    const mobileNav = document.querySelector('.mobile-nav')
+    const headerbtn = document.querySelector('.header__bars');
+    const mobileNav = document.querySelector('.mobile-nav');
+    const mobileLinks = document.querySelectorAll('.mobile-nav__link');
+
+    let isMobileNavOpen = false;
     
     headerbtn.addEventListener('click', () => {
-        mobileNav.style.display = 'flex';
+        isMobileNavOpen = !isMobileNavOpen;
+        if (isMobileNavOpen) {
+            mobileNav.style.display = 'flex';
+            document.body.style.overflowY = 'hidden';						
+        } else {
+            mobileNav.style.display = 'none';
+            document.body.style.overflowY = 'auto';						
+        }
+    });
+
+    mobileLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            isMobileNavOpen = false;
+            mobileNav.style.display = 'none';
+            document.body.style.overflowY = 'auto';
+        });
     });
 };
+
+mobileNav();
